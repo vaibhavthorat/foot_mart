9c70ff504dab98a997dfbe597fee27b4f9f7f641
vaibhavthorat115@gmail.com <vaibhavthorat115@gmail.com>
2023-04-19 22:08:21 +0530

    Updates

---
+++ precache-manifest.a2d8e95760065c45dcb5776b17b10d13.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec663933abf0c5740bfe",
+    "url": "/foot_mart/static/css/main.ba8d11c6.chunk.css"
+  },
+  {
+    "revision": "ec663933abf0c5740bfe",
+    "url": "/foot_mart/static/js/main.ec663933.chunk.js"
+  },
+  {
+    "revision": "1b67a3af7cabeb80a5b9",
+    "url": "/foot_mart/static/css/1.4febf4b7.chunk.css"
+  },
+  {
+    "revision": "1b67a3af7cabeb80a5b9",
+    "url": "/foot_mart/static/js/1.1b67a3af.chunk.js"
+  },
+  {
+    "revision": "faed3e4bfc6ca24659b2",
+    "url": "/foot_mart/static/js/runtime~main.faed3e4b.js"
+  },
+  {
+    "revision": "aef13526428d2121d46d45253cf48132",
+    "url": "/foot_mart/static/media/logo.aef13526.png"
+  },
+  {
+    "revision": "d1876d8d88d166ef29c6e23910e64cff",
+    "url": "/foot_mart/static/media/footer.d1876d8d.jpg"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/foot_mart/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/foot_mart/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/foot_mart/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/foot_mart/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/foot_mart/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "6585946c65e78db4a02ef3fb8e40c680",
+    "url": "/foot_mart/static/media/shose3.6585946c.jpg"
+  },
+  {
+    "revision": "112a420dc66c3822257f6019a32fb91c",
+    "url": "/foot_mart/static/media/shose5.112a420d.jpg"
+  },
+  {
+    "revision": "d94d94968634b6a876804631b73884c9",
+    "url": "/foot_mart/static/media/shose6.d94d9496.jpg"
+  },
+  {
+    "revision": "11e93c1330830faca47450dfad90264b",
+    "url": "/foot_mart/static/media/shose7.11e93c13.jpg"
+  },
+  {
+    "revision": "f484ce27933007d8b487d3a691949781",
+    "url": "/foot_mart/static/media/shose8.f484ce27.jpg"
+  },
+  {
+    "revision": "f9edada1fce1eca8d7f01f2c990de741",
+    "url": "/foot_mart/static/media/shose9.f9edada1.jpg"
+  },
+  {
+    "revision": "069a18767202464b35a033b150d9088b",
+    "url": "/foot_mart/static/media/shose10.069a1876.jpg"
+  },
+  {
+    "revision": "b0409d94c9b450950f0b28d2320477eb",
+    "url": "/foot_mart/static/media/shose11.b0409d94.jpg"
+  },
+  {
+    "revision": "5e76d620ed5256d1dd35d46f51e71134",
+    "url": "/foot_mart/static/media/shose12.5e76d620.jpg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/foot_mart/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/foot_mart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/foot_mart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/foot_mart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/foot_mart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f58d030114e22f880a45bf0b9cb41846",
+    "url": "/foot_mart/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b22a6f1674ddb3247ce81b745955f36.js"
+  "/foot_mart/precache-manifest.a2d8e95760065c45dcb5776b17b10d13.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/foot_mart/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
